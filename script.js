5a9d36efb982adce2c22a2cab4848b4d9050ae7f
richardhanitio <richardhan82@gmail.com>
2022-03-26 20:31:47 +0700

    make several changes

---
+++ script.js
@@ -0,0 +1,18 @@
+var headerMobile = document.querySelector(".header-list-down-container");
+var bar1 = document.getElementById("bar1");
+var bar2 = document.getElementById("bar2");
+var bar3 = document.getElementById("bar3");
+
+function isChecked() {
+    if (document.getElementById("check").checked) {
+        headerMobile.style.transform = "translateX(0)";
+        bar1.classList.toggle("anim1");
+        bar2.classList.toggle("anim2");
+        bar3.classList.toggle("anim3");
+    } else {
+        headerMobile.style.transform = "translateX(100%)";
+        bar1.classList.toggle("anim1");
+        bar2.classList.toggle("anim2");
+        bar3.classList.toggle("anim3");
+    }
+}
