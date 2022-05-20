ff01bbaa86d82152f5b36feaed74e47e4d797f49
richardhanitio <richardhan82@gmail.com>
2022-05-21 00:24:29 +0700

    formatted home

--- script.js
+++ script.js
@@ -1,22 +1,67 @@
-// var headerMobile = document.querySelector(".header-list-down-container");
-// var bar1 = document.getElementById("bar1");
-// var bar2 = document.getElementById("bar2");
-// var bar3 = document.getElementById("bar3");
+// variables
+const headerNavbarContainer = document.querySelector(
+    ".header-navbar-container"
+);
+const hamburgerIcon = document.querySelector(".header-hamburger-icon");
+const header = document.querySelector(".header");
+const headerNavbarList = [
+    ...headerNavbarContainer.querySelectorAll(".header-navbar-list"),
+];
 
-function isChecked() {
-    if (document.getElementById("check").checked) {
-        headerMobile.style.transform = "translateX(0)";
-        bar1.classList.toggle("anim1");
-        bar2.classList.toggle("anim2");
-        bar3.classList.toggle("anim3");
-    } else {
-        headerMobile.style.transform = "translateX(100%)";
-        bar1.classList.toggle("anim1");
-        bar2.classList.toggle("anim2");
-        bar3.classList.toggle("anim3");
-    }
-
-    
- 
-    
+// init swiper
+if (screen.width <= 700) {
+    let swiper = new Swiper(".mySwiper", {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        centeredSlides: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+} else {
+    let swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 }
+
+// click hamburger icon
+hamburgerIcon.addEventListener("click", () => {
+    headerNavbarContainer.classList.toggle("header-active");
+    headerNavbarList.forEach((list) =>
+        list.classList.toggle("header-navbar-list-anim")
+    );
+});
+
+// scroll down header
+window.addEventListener("scroll", () => {
+    const scrolled = window.scrollY;
+    if (screen.width > 700) {
+        if (scrolled >= 200) {
+            header.style.cssText = `
+                height : 4rem;
+                opacity : 0.6;
+                transition : all 0.3s;
+                box-shadow: 0 5px 10px 0 rgba(0,0,0,.5);
+            `;
+        } else {
+            header.style.cssText = `
+                height : 5rem;
+                opacity : 1;
+                transition : all 0.3s;
+            `;
+        }
+    }
+});
